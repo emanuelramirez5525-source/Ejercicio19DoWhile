32e5877944053c3146a2c71568e2b55604d1951b
emanu <emanu@EMANUEL_RAMREZ>
2026-03-05 23:06:56 -0600

    Logica Aplicada!

--- Program.cs
+++ Program.cs
@@ -1,1 +1,16 @@
 ﻿// Cree un programa que pida un número al usuario y muestre su tabla de multiplicar del 1 al 10 utilizando do-while.
+
+int num = 0;
+int tabla = 1;
+
+Console.WriteLine("Ingrese un número");
+num = int.Parse(Console.ReadLine());
+
+do
+{
+    Console.WriteLine($"{num} X {tabla} = {num * tabla}");
+    tabla++;
+
+
+} while (tabla <= 10);
+
